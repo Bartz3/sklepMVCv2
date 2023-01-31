5ba64b1ad96a0513fe56279f7343d4b748e97703
Bartek Późniewski <77844@student.pb.edu.pl>
2023-01-31 12:20:54 +0100

    added notification after order summary

---
+++ Scripts/showNotification.js
@@ -0,0 +1,10 @@
+﻿function showNotification() {
+    // Wyświetlenie powiadomienia
+    alert("Zamówienie zatwierdzone");
+
+    // Ustawienie timer na 5 sekund
+    setTimeout(function () {
+        // Ukrycie powiadomienia
+        alert("Powiadomienie zakończone");
+    }, 5000);
+}
